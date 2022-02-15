d8e0f1db450b44814b885eacf2f8c30318b11c78
Garrettwm12 <99695300+Garrettwm12@users.noreply.github.com>
2022-02-15 10:17:38 -0600

    customization, details, main screen

---
+++ app/src/main/java/com/example/pavinhaven/ListingCustomization.kt
@@ -0,0 +1,11 @@
+package com.example.pavinhaven
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ListingCustomization : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_listing_customization)
+    }
+}
