047540a92e8221900233f74e77939475476748e7
Garrettwm12 <99695300+Garrettwm12@users.noreply.github.com>
2022-02-14 18:42:34 -0600

    choose ,account, payment, listing

---
+++ app/src/main/java/com/example/pavinhaven/ChooseListing.kt
@@ -0,0 +1,11 @@
+package com.example.pavinhaven
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class ChooseListing : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_choose_listing)
+    }
+}
